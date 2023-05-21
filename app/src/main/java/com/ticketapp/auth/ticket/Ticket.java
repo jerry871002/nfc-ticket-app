2d536f4583efbba47488c17a289cf3d96112dcf9
Xu Zehui <zehuixu1999@gmail.com>
2023-05-21 18:55:32 +0300

    remove two-block data backup & reduce pages & resolve rollback ack * remove codes related to two-block data backup * reduce HMAC page to 1 page and the amount of pages to be wrote when issuing or using * add init_counter to check whether it's a real first use

--- app/src/main/java/com/ticketapp/auth/ticket/Ticket.java
+++ app/src/main/java/com/ticketapp/auth/ticket/Ticket.java
@@ -42,8 +42,9 @@ public class Ticket {
     private int expiryTime = 0;
     private static int lastUsedTime = 0;
     private int counter = 0;
+    private int initCounter = 0;
     private int maxCounterValue = 0;
-    private byte[] cardHMAC = new byte[8];
+    private byte[] cardHMAC = new byte[4];
 
     private static String infoToShow = "-"; // Use this to show messages
 
@@ -51,9 +52,11 @@ public class Ticket {
      * Data Structure
      */
     private static final int UID_PAGE = 0;
-    private static final int[] BOUND_PAGE = {4, 8};
-    private static final int[] EXPIRE_TIME_PAGE = {5, 9};
-    private static final int[] HMAC_PAGE = {6, 10};
+
+    private static final int BOUND_PAGE = 4;
+    private static final int INIT_COUNTER_PAGE = 5;
+    private static final int EXPIRE_TIME_PAGE = 6;
+    private static final int HMAC_PAGE = 7;
     private static final int COUNTER_PAGE = 41;
     private static final int AUTH0_PAGE = 42;
     private static final int AUTH1_PAGE = 43;
@@ -115,7 +118,7 @@ public class Ticket {
 
     private byte[] generateKey(byte[] uid) {
         byte[] key = new byte[KEY_LENGTH];
-        PBEKeySpec spec = new PBEKeySpec(new String(defaultHMACKey).toCharArray(), uid, 1000, 256);
+        PBEKeySpec spec = new PBEKeySpec(new String(authenticationKey).toCharArray(), uid, 1000, 256);
         try {
             SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
             byte[] hash = keyFactory.generateSecret(spec).getEncoded();
@@ -129,7 +132,7 @@ public class Ticket {
 
     private byte[] generateHMACKey(byte[] uid) {
         byte[] key = new byte[KEY_LENGTH];
-        PBEKeySpec spec = new PBEKeySpec(new String(authenticationKey).toCharArray(), uid, 1000, 256);
+        PBEKeySpec spec = new PBEKeySpec(new String(hmacKey).toCharArray(), uid, 1000, 256);
         try {
             SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
             byte[] hash = keyFactory.generateSecret(spec).getEncoded();
@@ -183,15 +186,13 @@ public class Ticket {
         return true;
     }
 
-    private boolean getTicketData(int block) {
+    private boolean getTicketData() {
         byte[] maxCounterValueArr = new byte[4];
         byte[] expiryTimeArr = new byte[4];
-
-        System.out.println("block:" + block);
         try {
-            utils.readPages(BOUND_PAGE[block], 1, maxCounterValueArr, 0);
-            utils.readPages(EXPIRE_TIME_PAGE[block], 1, expiryTimeArr, 0);
-            utils.readPages(HMAC_PAGE[block], 2, cardHMAC, 0);
+            utils.readPages(BOUND_PAGE, 1, maxCounterValueArr, 0);
+            utils.readPages(EXPIRE_TIME_PAGE, 1, expiryTimeArr, 0);
+            utils.readPages(HMAC_PAGE, 1, cardHMAC, 0);
         } catch (Exception e) {
             Utilities.log("getTicketData, read pages error", true);
             return false;
@@ -209,12 +210,14 @@ public class Ticket {
     private byte[] computeHMAC() {
         byte[] maxCounterValueArr = toByteArr(maxCounterValue);
         byte[] expiryTimeArr = toByteArr(expiryTime);
+        byte[] initCounterArr = toByteArr(initCounter);
 
         ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
         try {
             dataStream.write(uid);
             dataStream.write(maxCounterValueArr);
             dataStream.write(expiryTimeArr);
+            dataStream.write(initCounterArr);
         } catch (Exception e) {
             Utilities.log("writeTicketData, compute HMAC value error", true);
             return new byte[20];
@@ -222,28 +225,19 @@ public class Ticket {
         return macAlgorithm.generateMac(dataStream.toByteArray());
     }
 
-    private boolean writeTicketData() {
-        int block = counter % 2;
+    private boolean writeTicketData(boolean maxCounterFlag, boolean expiryTimeFlag, boolean initCounterFlag, boolean HMACFlag) {
         boolean res = true;
         byte[] maxCounterValueArr = toByteArr(maxCounterValue);
         byte[] expiryTimeArr = toByteArr(expiryTime);
-        byte[] writeHMAC = Arrays.copyOfRange(computeHMAC(), 0, 8);
-        // Write data to current block
+        byte[] initCounterArr = toByteArr(initCounter);
+        byte[] writeHMAC = Arrays.copyOfRange(computeHMAC(), 0, 4);
         try {
-            res &= utils.writePages(maxCounterValueArr, 0, BOUND_PAGE[block], 1);
-            res &= utils.writePages(expiryTimeArr, 0, EXPIRE_TIME_PAGE[block], 1);
-            res &= utils.writePages(writeHMAC, 0, HMAC_PAGE[block], 2);
+            if(maxCounterFlag) res &= utils.writePages(maxCounterValueArr, 0, BOUND_PAGE, 1);
+            if(initCounterFlag) res &= utils.writePages(initCounterArr, 0, INIT_COUNTER_PAGE, 1);
+            if(expiryTimeFlag) res &= utils.writePages(expiryTimeArr, 0, EXPIRE_TIME_PAGE, 1);
+            if(HMACFlag) res &= utils.writePages(writeHMAC, 0, HMAC_PAGE, 1);
         } catch (Exception e) {
             Utilities.log("write data error", true);
-        }
-        // Backup data to the other block
-        block = (block + 1) % 2;
-        try {
-            res &= utils.writePages(maxCounterValueArr, 0, BOUND_PAGE[block], 1);
-            res &= utils.writePages(expiryTimeArr, 0, EXPIRE_TIME_PAGE[block], 1);
-            res &= utils.writePages(writeHMAC, 0, HMAC_PAGE[block], 2);
-        } catch (Exception e) {
-            Utilities.log("backup data error", true);
         }
         return res;
     }
@@ -263,13 +257,13 @@ public class Ticket {
 
         // fetch NFC card data
         getCounter();
-        getTicketData(counter % 2);
+        getTicketData();
 
         if (blank) {
             initBlankCard(authKey);
             maxCounterValue = counter + RIDES_PER_ISSUE;
-            expiryTime = 0;
-            if(!writeTicketData()) {
+            initCounter = counter;
+            if(!writeTicketData(true, false, true, true)) {
                 infoToShow = "Failed to issue ticket. Move too fast.";
                 return false;
             }
@@ -284,44 +278,10 @@ public class Ticket {
         }
 
         // check HMAC (Integrity)
-        byte[] computedHMAC = Arrays.copyOfRange(computeHMAC(), 0, 8);
+        byte[] computedHMAC = Arrays.copyOfRange(computeHMAC(), 0, 4);
         boolean integrity = Arrays.equals(cardHMAC, computedHMAC);
-        boolean resetCard = false;
-
-        // if data is broken in one block, caused by crashed write, check the other block
-        if (!integrity) {
-            Utilities.log("Current Block is Broken", true);
-            getTicketData((counter + 1) % 2);
-            computedHMAC = Arrays.copyOfRange(computeHMAC(), 0, 8);
-            integrity = Arrays.equals(cardHMAC, computedHMAC);
-            // check if data in the other block is valid
-            if (integrity) {
-                Utilities.log("Data restored from another block", true);
-            } else {
-                // two blocks are both broken
-                Utilities.log("Move too fast, or two blocks are both broken.", true);
-                resetCard = true;
-            }
-        }
-
-        if (resetCard) {
-            maxCounterValue = counter;
-            lastUsedTime = 0;
-            expiryTime = 0;
-        }
-
-        // check if the issued rides have expired
-        if (expiryTime < (int) (System.currentTimeMillis() / 1000) && expiryTime != 0) {
-            expiryTime = 0;
-            maxCounterValue = counter;
-        }
-
-        maxCounterValue += RIDES_PER_ISSUE;
-        remainingUses = maxCounterValue - counter;
-
-        /** Preparations all done, issue ticket*/
-        if(!writeTicketData()) {
-            infoToShow = "Failed to issue ticket. Move too fast.";
+        if(!integrity) {
+            infoToShow = "Card is broken.";
             return false;
         }
 
@@ -331,12 +291,29 @@ public class Ticket {
             return false;
         }
 
-        lastUsedTime = (int) (System.currentTimeMillis() / 1000);
-
-        if(resetCard) {
-            infoToShow = "Card is broken and has been reset.\n" + RIDES_PER_ISSUE + " tickets have been issued.\nTotal: 2 rides";
+        // check if the issued rides have expired or used up
+        if (maxCounterValue == counter || expiryTime < (int) (System.currentTimeMillis() / 1000) && expiryTime != 0) {
+            expiryTime = 0;
+            maxCounterValue = counter + RIDES_PER_ISSUE;
+            initCounter = counter;
+            if(!writeTicketData(true, true, true, true)) {
+                infoToShow = "Failed to issue ticket. Move too fast.";
+                return false;
+            }
+            infoToShow = "Card used up / expired.\n" + RIDES_PER_ISSUE + " new tickets have been issued.";
             return true;
         }
+
+        maxCounterValue += RIDES_PER_ISSUE;
+        remainingUses = maxCounterValue - counter;
+
+        /** Preparations all done, issue ticket*/
+        if(!writeTicketData(true, false, false, true)) {
+            infoToShow = "Failed to issue ticket. Move too fast.";
+            return false;
+        }
+
+        lastUsedTime = (int) (System.currentTimeMillis() / 1000);
 
         infoToShow = "Card in use.\n" + RIDES_PER_ISSUE + " more tickets have been issued.\nTotal: " + remainingUses + " rides";
         return true;
@@ -365,26 +342,14 @@ public class Ticket {
 
         // fetch NFC card data
         getCounter();
-        getTicketData(counter % 2);
+        getTicketData();
 
         // check HMAC (Integrity)
-        byte[] computedHMAC = Arrays.copyOfRange(computeHMAC(), 0, 8);
+        byte[] computedHMAC = Arrays.copyOfRange(computeHMAC(), 0, 4);
         boolean integrity = Arrays.equals(cardHMAC, computedHMAC);
-
-        // if data is broken in one block, caused by crashed write, check the other block
-        if (!integrity) {
-            Utilities.log("Current Block is Broken", true);
-            getTicketData((counter + 1) % 2);
-            computedHMAC = Arrays.copyOfRange(computeHMAC(), 0, 8);
-            integrity = Arrays.equals(cardHMAC, computedHMAC);
-            // check if data in the other block is valid
-            if (integrity) {
-                Utilities.log("Data restored from another block", true);
-            } else {
-                // two blocks are both broken
-                infoToShow = "Move too fast, or two blocks are both broken.";
-                return false;
-            }
+        if(!integrity) {
+            infoToShow = "Card is broken.";
+            return false;
         }
 
         SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy' 'HH:mm:ss");
@@ -402,18 +367,17 @@ public class Ticket {
             return false;
         }
 
-        if (lastUsedTime > System.currentTimeMillis() / 1000 - 3) {
+        if (lastUsedTime > System.currentTimeMillis() / 1000 - 2) {
             Utilities.log("Too fast, please wait and use card again", true);
-            infoToShow = "Please wait and use card again.";
+            infoToShow = "Too fast, please wait and use card again";
             return false;
         }
 
         remainingUses = maxCounterValue - counter - 1;
         lastUsedTime = (int) (System.currentTimeMillis() / 1000);
-        if (expiryTime == 0) {
+        if (initCounter == counter && expiryTime == 0) {
             expiryTime = lastUsedTime + EXPIRE_TIME;
-            counter++;
-            if(!writeTicketData()) {
+            if(!writeTicketData(false, true, false, true)) {
                 infoToShow = "Failed to issue ticket. Move too fast.";
                 return false;
             }
